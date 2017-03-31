381c16c1ff7f5532416522bbb76cb87ec0296bc6
Andrey Mazuro <andrey.mazuro@icloud.com>
2017-03-31 05:51:19 +0300

    fixed appearing button bug

--- src/App.js
+++ src/App.js
@@ -1,61 +1,45 @@
 import React, { Component } from 'react';
-import client from 'google-client-api'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import {IconMenu, MenuItem, IconButton, AppBar, Avatar} from 'material-ui';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import {getUserName, getImageUrl} from './authUtils'
+import {getUserInfo, authorize, signOut} from './authUtils'
 import './App.css';
+
 injectTapEventPlugin();
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state= {
+
+  constructor(props){
+    super(props)
+    this.state = {
       authorizedUser: null
     }
   }
 
   componentDidMount() {
-    this.renderSignInButton()
-  }
-
-  renderSignInButton() {
-    client()
-    .then((gapi) =>  {
-      gapi.signin2.render('google-signin-button', {
-        'scope': 'https://www.googleapis.com/auth/plus.login',
-        'width': 250,
-        'height': 50,
-        'longtitle': true,
-        'margin-left': 100,
-        'onsuccess': this.onSignIn
-      })
-    })
+    authorize(this.onSignIn)
   }
 
   isAuthorized = () => this.state.authorizedUser !== null
 
-  showUserName = (user) => {
-    const authorizedUser = getUserName(user)
-    const image = getImageUrl(user)
-    this.setState({authorizedUser: {name:authorizedUser, image: image}})
-  }
-
   onSignIn = (googleUser) => {
-    this.showUserName(googleUser)
-  }
-
-  hideNameAndButton = () => {
+    const { imageUrl, username } = getUserInfo(googleUser)
     this.setState({
-      authorizedUser: null
+      authorizedUser: {
+        name: username,
+        image: imageUrl
+      }
     })
   }
 
-  signOut = () => {
-    client()
-    .then((gapi) => gapi.auth2.getAuthInstance().signOut())
-    .then(this.hideNameAndButton())
+  onSignOut = () => {
+    signOut().then(()=> {
+      this.setState({
+        authorizedUser: null
+      })
+      authorize(this.onSignIn)
+    })
   }
 
   render() {
@@ -63,14 +47,14 @@ class App extends Component {
       <MuiThemeProvider>
         <div>
           <AppBar
-            title={this.isAuthorized()? this.state.authorizedUser.name : null}
-            iconElementLeft={this.isAuthorized()? <Avatar src={this.state.authorizedUser.image} /> : null}
-            iconElementRight={this.isAuthorized()? <UserMenu signOut={this.signOut}/> : null}
+            title={this.isAuthorized() ? this.state.authorizedUser.name : null}
+            iconElementLeft={this.isAuthorized() ? <Avatar src={this.state.authorizedUser.image} /> : null}
+            iconElementRight={this.isAuthorized() ? <UserMenu onSignOut={this.onSignOut}/> : null}
           />
-        <div id="google-signin-button" className={this.isAuthorized()? 'hidden':'main-btn'}></div>
-      </div>
-    </MuiThemeProvider>
-    );
+          <div id="google-signin-button" className={this.isAuthorized() ? 'btn-hidden' : 'main-btn'}></div>
+        </div>
+      </MuiThemeProvider>
+    )
   }
 }
 
@@ -79,14 +63,12 @@ const UserMenu = (props) => (
   <IconMenu
     iconButtonElement={
       <IconButton><MoreVertIcon /></IconButton>
-    }
+                      }
     targetOrigin={{horizontal: 'right', vertical: 'top'}}
     anchorOrigin={{horizontal: 'right', vertical: 'top'}}
   >
-    <MenuItem primaryText="Log out" onClick={props.signOut}/>
+    <MenuItem primaryText="Log out" onClick={props.onSignOut}/>
   </IconMenu>
 );
-
-UserMenu.muiName = 'IconMenu';
 
 export default App;
--- src/authUtils.js
+++ src/authUtils.js
@@ -1,9 +1,34 @@
-export const getUserName = (googleUser) => {
-  var profile = googleUser.getBasicProfile()
-  return profile.getName()
+export const getUserInfo = (googleUser) => {
+  const profile = googleUser.getBasicProfile()
+  const imageUrl = profile.getImageUrl()
+  const username = profile.getName()
+  return { imageUrl, username }
 }
 
-export const getImageUrl = (googleUser) => {
-  var profile = googleUser.getBasicProfile()
-  return profile.getImageUrl()
+const renderSignInButton = (onSignIn) => {
+  window.gapi.signin2.render('google-signin-button', {
+    'scope': 'https://www.googleapis.com/auth/plus.login',
+    'width': 250,
+    'height': 50,
+    'longtitle': true,
+    'margin-left': 100,
+    'onsuccess': onSignIn
+  })
+}
+
+export const authorize = (onSignIn) => {
+  window.gapi.load("client:auth2", () => {
+    window.gapi.auth2.init({client_id: '370428235529-6a542uppjqph0jhgaa1n8vhihmb77c8r.apps.googleusercontent.com'})
+    window.gapi.auth2.getAuthInstance().then((auth) => {
+      if (auth.isSignedIn.get()) {
+        onSignIn(auth.currentUser.get())
+      } else {
+        renderSignInButton(onSignIn)
+      }
+    })
+  })
+}
+
+export const signOut = () => {
+  return window.gapi.auth2.getAuthInstance().signOut()
 }
